00089ef86ca1687e5270373092627e4c40fe75d4
Black Source <64044813+BLACKBOTSS@users.noreply.github.com>
2020-10-24 18:38:31 +0300

    Update ReplyBot.lua

--- files_BLACKBOTSS/ReplyBot.lua
+++ files_BLACKBOTSS/ReplyBot.lua
@@ -285,13 +285,13 @@ end
 
 if text == 'تفعيل ردود البوت' and Owner(msg) then
 database:del(bot_id..'BLACKBOTSS:Reply:Mute'..msg.chat_id_)
-send(msg.chat_id_, msg.id_,'☑┇تم تفعيل ردود البوت')
+send(msg.chat_id_, msg.id_,'⌔︙تم تفعيل ردود البوت')
 return false
 end
 
 if text == 'تعطيل ردود البوت' and Owner(msg) then
 database:set(bot_id..'BLACKBOTSS:Reply:Mute'..msg.chat_id_,true)
-send(msg.chat_id_, msg.id_,'☑┇تم تعطيل ردود البوت')
+send(msg.chat_id_, msg.id_,'⌔︙تم تعطيل ردود البوت')
 return false
 end
 
